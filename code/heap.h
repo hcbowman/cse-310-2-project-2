af0f7ed5b89d5a6b23a042ea18d6cc5e9a02cc4c
Hunter Bowman <hbowman877@hotmail.com>
2019-10-17 03:29:09 -0700

    made some headers, started on file input

---
+++ code/heap.h
@@ -0,0 +1,28 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+class heap
+{
+private:
+
+    struct node
+    {
+        
+    };
+    
+
+public:
+    heap(/* args */);
+    ~heap();
+};
+
+heap::heap(/* args */)
+{
+}
+
+heap::~heap()
+{
+}
+
+
+#endif
--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,36 @@
 #include<iostream>
+#include<vector>
+#include<string>
+#include"defn.h"
 
-int main () {
+int main (int argc, char** argv) {
 
-    std::cout << "DEBUG: Hello Project2" << "\n";
+    //Variables
+
+
+    //Objects
+    std::vector<std::string> dates_vector; //Holds the range of years inputed via command line
+
+
+    //Disable synchronization between the C and C++ standard streams
+	std::ios::sync_with_stdio(false);
+
+    //Get command line args
+    //TODO: change argv to argc maybe
+    if (argc != 1) {
+
+        for (int i = 2; i < argc; i++) {
+            //dates_vector.push_back( std::stoi(argv[i]) );
+            dates_vector.push_back(argv[i]);
+        }                
+
+    }
+    else {
+        return -1;
+    }
+
+
+
+    std::cout << "DEBUG: end Project2" << "\n";
 
 }
